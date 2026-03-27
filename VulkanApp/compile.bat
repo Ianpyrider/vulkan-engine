757c3c9043d9012ef8949603a773d3dd568e8caf
Ianpyrider <Ianpyrider@users.noreply.github.com>
2026-03-27 16:39:45 -0700

    Migrate to CMake and clean up

--- VulkanApp/compile.bat
+++ VulkanApp/compile.bat
@@ -1,4 +1,22 @@
+@echo off
+
 cd /D "%~dp0"
 
-C:/VulkanSDK/1.4.321.1/bin/slangc.exe Shaders/shader.slang -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -entry vertMain -entry fragMain -o slang.spv
-C:/VulkanSDK/1.4.321.1/bin/slangc.exe Shaders/n64_compute.slang -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -entry compMain -o compute.spv
+set "OUT_DIR=%~1"
+
+:: Fallback for no cmake
+if "%OUT_DIR%"=="" set "OUT_DIR=."
+
+echo --- Compiling Slang Shaders to: %OUT_DIR% ---
+
+"C:/VulkanSDK/1.4.321.1/bin/slangc.exe" Shaders/shader.slang ^
+    -target spirv -profile spirv_1_4 -emit-spirv-directly ^
+    -fvk-use-entrypoint-name -entry vertMain -entry fragMain ^
+    -o "%OUT_DIR%/slang.spv"
+
+"C:/VulkanSDK/1.4.321.1/bin/slangc.exe" Shaders/n64_compute.slang ^
+    -target spirv -profile spirv_1_4 -emit-spirv-directly ^
+    -fvk-use-entrypoint-name -entry compMain ^
+    -o "%OUT_DIR%/compute.spv"
+
+echo --- Shader Compilation Complete ---
